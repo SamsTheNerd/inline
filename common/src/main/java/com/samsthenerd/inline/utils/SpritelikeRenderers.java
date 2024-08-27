b1723ac840d026094f7e03fa0af6e695054e1ce2
SamsTheNerd <sam@samsthenerd.com>
2024-08-27 19:47:53 -0400

    make spritelikes fade properly

--- common/src/main/java/com/samsthenerd/inline/utils/SpritelikeRenderers.java
+++ common/src/main/java/com/samsthenerd/inline/utils/SpritelikeRenderers.java
@@ -1,24 +1,14 @@
 package com.samsthenerd.inline.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.joml.Matrix4f;
-
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.samsthenerd.inline.utils.Spritelike.SpritelikeType;
-
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.BufferRenderer;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.*;
 import net.minecraft.util.Identifier;
+import org.joml.Matrix4f;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpritelikeRenderers {
     private static final Map<SpritelikeType, SpritelikeRenderer> RENDERER_MAP = new HashMap<>();
@@ -44,16 +34,17 @@ public class SpritelikeRenderers {
         public void drawSpriteWithLight(Spritelike sprite, DrawContext ctx, float x, float y, float z, float width, float height, int light, int argb) {
             Identifier texture = sprite.getTextureId();
             RenderSystem.setShaderTexture(0, texture);
-            RenderSystem.setShader(GameRenderer::getRenderTypeEntityCutoutProgram);
+            RenderSystem.setShader(GameRenderer::getRenderTypeEntityTranslucentProgram);
             Matrix4f matrix4f = ctx.getMatrices().peek().getPositionMatrix();
             // BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-            VertexConsumer vc = ctx.getVertexConsumers().getBuffer(RenderLayer.getEntityCutout(texture));
+            RenderLayer renderLayer = RenderLayer.getEntityTranslucent(texture);
+            VertexConsumer vc = ctx.getVertexConsumers().getBuffer(renderLayer);
             vc.vertex(matrix4f, x, y, z).color(argb).texture(sprite.getMinU(), sprite.getMinV()).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(ctx.getMatrices().peek().getNormalMatrix(), 1f, 1f, 1f).next();
             vc.vertex(matrix4f, x, y+height, z).color(argb).texture(sprite.getMinU(), sprite.getMaxV()).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(ctx.getMatrices().peek().getNormalMatrix(), 1f, 1f, 1f).next();
             vc.vertex(matrix4f, x+width, y+height, z).color(argb).texture(sprite.getMaxU(), sprite.getMaxV()).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(ctx.getMatrices().peek().getNormalMatrix(), 1f, 1f, 1f).next();
             vc.vertex(matrix4f, x+width, y, z).color(argb).texture(sprite.getMaxU(), sprite.getMinV()).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(ctx.getMatrices().peek().getNormalMatrix(), 1f, 1f, 1f).next();
 
-            ctx.getVertexConsumers().draw();
+            ctx.getVertexConsumers().draw(renderLayer);
         }
         
         public void drawSprite(Spritelike sprite, DrawContext ctx, float x, float y, float z, float width, float height){
