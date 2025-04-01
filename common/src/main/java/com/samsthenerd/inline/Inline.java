bbf7ddfba5e9acc969f3edb676e66fe3fd580ce5
SamsTheNerd <sam@samsthenerd.com>
2025-04-01 19:00:23 -0400

    fix showoff matcher packet issues

--- common/src/main/java/com/samsthenerd/inline/Inline.java
+++ common/src/main/java/com/samsthenerd/inline/Inline.java
@@ -10,17 +10,20 @@ import com.samsthenerd.inline.api.data.SpriteInlineData.SpriteDataType;
 import com.samsthenerd.inline.api.matching.InlineMatch;
 import com.samsthenerd.inline.api.matching.MatcherInfo;
 import com.samsthenerd.inline.api.matching.RegexMatcher;
+import com.samsthenerd.inline.api.matching.RegexMatcher.Standard;
 import com.samsthenerd.inline.xplat.XPlatInstances;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 // this will probably be bumped out into its own mod Soon, but i want to get it working in this test environment first
 public class Inline {
@@ -59,20 +62,22 @@ public class Inline {
         InlineAPI.INSTANCE.addDataType(SpriteDataType.INSTANCE);
     }
 
+    private static final Map<String, String> EQUIPMENT_ALIASES = Map.of(
+        "hand", "mainhand", "chestplate", "chest", "leggings", "legs", "helmet", "head",
+        "boots", "feet", "shoes", "feet"
+    );
     private static void addChatMatchers(){
         Identifier showOffID = Identifier.of(Inline.MOD_ID, "showoff");
-        RegexMatcher showOffMatcher = new RegexMatcher.ChatStandard("show", "hand|offhand", showOffID,
+        RegexMatcher showOffMatcher = new RegexMatcher.ChatStandard("show", Standard.IDENTIFIER_REGEX_INSENSITIVE, showOffID,
                 (whatToShow, ctx) -> {
-                    ItemStack stack = new ItemStack(Items.AIR);
-                    stack.set(DataComponentTypes.CUSTOM_NAME, Text.literal("Nothing to showoff"));
-                    if(whatToShow.equals("hand")){
-                        stack = ctx.getChatSender().getStackInHand(Hand.MAIN_HAND);
-                    } else if(whatToShow.equals("offhand")){
-                        stack = ctx.getChatSender().getStackInHand(Hand.OFF_HAND);
-                    }
-                    if(stack.isEmpty()){
-                        stack = new ItemStack(Items.AIR);
-                        stack.set(DataComponentTypes.CUSTOM_NAME, Text.literal("Nothing to showoff"));
+                    ItemStack stack = null;
+                    whatToShow = EQUIPMENT_ALIASES.getOrDefault(whatToShow, whatToShow);
+                    try{
+                        stack = ctx.getChatSender().getEquippedStack(EquipmentSlot.byName(whatToShow));
+                    } catch (Exception e){}
+                    if(stack == null || stack.isEmpty()){
+                        stack = new ItemStack(Items.BARRIER);
+                        stack.set(DataComponentTypes.CUSTOM_NAME, Text.translatable("ui.inline.nothingtoshowoff"));
                     }
                     HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_ITEM, new HoverEvent.ItemStackContent(stack));
                     return new InlineMatch.DataMatch(new ItemInlineData(stack), Style.EMPTY.withHoverEvent(he));
